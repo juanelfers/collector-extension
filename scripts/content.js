9c7ed61213e6785cfd944fe44dc6ae1ed255c5be
Juan Elfers <juan.elfers@gmail.com>
2023-06-18 21:57:58 -0300

    Scraping script

--- scripts/content.js
+++ scripts/content.js
@@ -1,22 +1,37 @@
-const article = document.querySelector("article");
+const Extension = {
+    cardContainer: 'div.card',
 
-// `document.querySelector` may return null if the selector doesn't match anything.
-if (article) {
-    const text = article.textContent;
-    const wordMatchRegExp = /[^\s]+/g; // Regular expression
-    const words = text.matchAll(wordMatchRegExp);
-    // matchAll returns an iterator, convert to array to get word count
-    const wordCount = [...words].length;
-    const readingTime = Math.round(wordCount / 200);
-    const badge = document.createElement("p");
-    // Use the same styling as the publish information in an article's header
-    badge.classList.add("color-secondary-text", "type--caption");
-    badge.textContent = `⏱️ ${readingTime} min read`;
+    init () {
+        const collection = this.checkCollection();
+        console.log({ collection });
+    },
 
-    // Support for API reference docs
-    const heading = article.querySelector("h1");
-    // Support for article docs with date
-    const date = article.querySelector("time")?.parentNode;
+    checkCollection () {
+        const cards = document.querySelector('.cards');
 
-    (date ?? heading).insertAdjacentElement("afterend", badge);
-}
+        if (!cards) return null;
+
+        const cardList = Array.from(document.querySelectorAll(this.cardContainer)).map(card => {
+            const [cardNumber, cardName] = card.querySelector('.plaque').innerText.split(' - ');
+            const cardImage = card.querySelector('img').src;
+
+            return {
+                cardNumber,
+                cardName,
+                cardImage
+            }
+        });
+
+        const collectionName = document.querySelector('#siteBody h1').textContent;
+        const cardQuantity = +document.querySelectorAll('.cards')?.[0]?.innerText.match(/[0-9]+/)[0];
+        const secretCardQuantity = cardList.length - cardQuantity;
+
+        return {
+            cardQuantity,
+            secretCardQuantity,
+            cardList,
+        }
+    }
+};
+
+Extension.init();
